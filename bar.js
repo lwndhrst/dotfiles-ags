901723cfae8fdbc600792ad4b2c488e40db1165f
Leon Windhorst <leon@windhorst.dev>
2024-11-03 09:16:30 +0100

    Update bar

--- bar.js
+++ bar.js
@@ -1,16 +1,9 @@
 const hyprland = await Service.import("hyprland")
-const mpris = await Service.import("mpris")
-const audio = await Service.import("audio")
-const battery = await Service.import("battery")
 const systemtray = await Service.import("systemtray")
 
 const date = Variable("", {
     poll: [1000, 'date "+%H:%M %b%e, %Y"'],
 })
-
-// widgets can be only assigned as a child in one container
-// so to make a reuseable widget, make it a function
-// then you can simply instantiate one by calling it
 
 function Workspaces() {
     const activeId = hyprland.active.workspace.bind("id")
@@ -18,108 +11,21 @@ function Workspaces() {
         .as(ws => ws.map(({ id }) => Widget.Button({
             on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
             child: Widget.Label(`${id}`),
-            class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
+            class_name: activeId.as(i => `${i === id ? "workspace-active" : "workspace-inactive"}`),
         })))
 
     return Widget.Box({
-        class_name: "workspaces",
+        class_name: "bar-workspaces",
         children: workspaces,
     })
 }
 
-
-function ClientTitle() {
-    return Widget.Label({
-        class_name: "client-title",
-        label: hyprland.active.client.bind("title"),
-    })
-}
-
-
 function Clock() {
     return Widget.Label({
-        class_name: "clock",
+        class_name: "bar-clock",
         label: date.bind(),
     })
 }
-
-
-function Media() {
-    const label = Utils.watch("", mpris, "player-changed", () => {
-        if (mpris.players[0]) {
-            const { track_artists, track_title } = mpris.players[0]
-            return `${track_artists.join(", ")} - ${track_title}`
-        } else {
-            return "Nothing is playing"
-        }
-    })
-
-    return Widget.Button({
-        class_name: "media",
-        on_primary_click: () => mpris.getPlayer("")?.playPause(),
-        on_scroll_up: () => mpris.getPlayer("")?.next(),
-        on_scroll_down: () => mpris.getPlayer("")?.previous(),
-        child: Widget.Label({ label }),
-    })
-}
-
-
-function Volume() {
-    const icons = {
-        101: "overamplified",
-        67: "high",
-        34: "medium",
-        1: "low",
-        0: "muted",
-    }
-
-    function getIcon() {
-        const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
-            threshold => threshold <= audio.speaker.volume * 100)
-
-        return `audio-volume-${icons[icon]}-symbolic`
-    }
-
-    const icon = Widget.Icon({
-        icon: Utils.watch(getIcon(), audio.speaker, getIcon),
-    })
-
-    const slider = Widget.Slider({
-        hexpand: true,
-        draw_value: false,
-        on_change: ({ value }) => audio.speaker.volume = value,
-        setup: self => self.hook(audio.speaker, () => {
-            self.value = audio.speaker.volume || 0
-        }),
-    })
-
-    return Widget.Box({
-        class_name: "volume",
-        css: "min-width: 180px",
-        children: [icon, slider],
-    })
-}
-
-
-function BatteryLabel() {
-    const value = battery.bind("percent").as(p => p > 0 ? p / 100 : 0)
-    const icon = battery.bind("percent").as(p =>
-        `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
-
-    return Widget.Box({
-        class_name: "battery",
-        visible: battery.bind("available"),
-        children: [
-            Widget.Icon({ icon }),
-            Widget.LevelBar({
-                widthRequest: 140,
-                vpack: "center",
-                value,
-            }),
-        ],
-    })
-}
-
 
 function SysTray() {
     const items = systemtray.bind("items")
@@ -131,18 +37,16 @@ function SysTray() {
         })))
 
     return Widget.Box({
+        class_name: "bar-systray",
         children: items,
     })
 }
 
-
-// layout of the bar
 function Left() {
     return Widget.Box({
         spacing: 8,
         children: [
             Workspaces(),
-            // ClientTitle(),
         ],
     })
 }
@@ -151,7 +55,6 @@ function Center() {
     return Widget.Box({
         spacing: 8,
         children: [
-            // Media(),
         ],
     })
 }
@@ -161,8 +64,6 @@ function Right() {
         hpack: "end",
         spacing: 8,
         children: [
-            // Volume(),
-            BatteryLabel(),
             SysTray(),
             Clock(),
         ],
@@ -178,7 +79,7 @@ export function Bar(monitor = 0) {
         exclusivity: "exclusive",
         child: Widget.CenterBox({
             start_widget: Left(),
-            center_widget: Center(),
+            // center_widget: Center(),
             end_widget: Right(),
         }),
     })
